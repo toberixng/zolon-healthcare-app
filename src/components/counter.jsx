5f2d858fc7afec2e7a3fdeee277ff0af131f4e1c
toberixng <tobyfatumo@gmail.com>
2019-03-14 14:32:50 +0100

    render update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+         count: 1
+    };
+
     render() { 
-        return <h1>Hello World</h1>;
+        return (
+        <div>
+            <span class={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button  class="btn btn-secondary btn-sm">Increment</button>
+        </div>
+        );
+    }
+
+    getBadgeClasses () {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
     }
 }
  
